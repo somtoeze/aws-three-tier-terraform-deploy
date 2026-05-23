459ba18c84ea604c4eeef7f2525bc0d8cf7d8ede
somto <somtochukwueze95@gmail.com>
2026-05-23 03:40:28 +0100

    first commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myapp-tf-state-backend "
+    bucket = "myapp-tf-state-backend"
     key    = "prodution/terraform.tfstate"
     region = "us-east-1"
   }
